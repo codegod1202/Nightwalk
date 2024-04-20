c8138c61c5f2a242649e112091c67167cf803857
Adi-462 <adityamaurya462@gmail.com>
2024-04-21 02:47:15 +0530

    almost final

--- Assets/Scripts/enemy/DamagePlayer.cs
+++ Assets/Scripts/enemy/DamagePlayer.cs
@@ -17,7 +17,7 @@ public class DamagePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < 10f)
+        if (Vector3.Distance(transform.position, player.transform.position) < 3f)
         {
             // Decrement the number every 2 seconds
             if (Time.time % 2f < Time.deltaTime)
